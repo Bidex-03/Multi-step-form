c067ab8535ac52a311207e8d76f8d239ca0a7dd4
Bidex-03 <abdullahqaasim14@gmail.com>
2023-02-19 00:45:00 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import CustomButton from "./components/CustomButton";
+
+function App() {
+  return <CustomButton />
+}
+
+export default App;
---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,9 @@
+import classes from "../css/CustomButton.module.css"
+
+const CustomButton = props => {
+    return (
+        <h1 className="bg-red-500">Let's get started</h1>
+    )
+}
+
+export default CustomButton;
