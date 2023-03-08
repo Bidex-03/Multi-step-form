341fb4184c52d05e598a665811ec1a58796491ed
Bidex-03 <abdullahqaasim14@gmail.com>
2023-03-08 13:32:34 +0100

    Fixed the toggle switch

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -5,15 +5,15 @@ const Toggle = ({ onClick }) => {
     <>
       <label
         for="check"
-        class="bg-Marine-blue relative w-12 h-6 rounded-full cursor-pointer"
+        className="bg-Marine-blue relative w-12 h-6 rounded-full cursor-pointer"
       >
         <input
           type="checkbox"
           id="check"
-          class="sr-only peer"
+          className="sr-only peer"
           onClick={onClick}
         />
-        <span class="w-2/5 h-4/5 bg-White absolute rounded-full left-1 top-0.5 peer-checked:left-6 transition-all duration-300"></span>
+        <span className="w-2/5 h-4/5 bg-White absolute rounded-full left-1 top-0.5 peer-checked:left-6 transition-all duration-300"></span>
       </label>
     </>
   );
